4285b2aa443696633cddf8b2e9dc701bbcda105a
InnaLyksa <108470037+InnaLyksa@users.noreply.github.com>
2022-09-19 07:02:27 +0000

    Deploying to gh-pages from @ InnaLyksa/ice-cream-teamFirst@07aec653f8e778ed818c100a8762ad35957ed106 🚀

--- index.bfcdc6e9.js
+++
@@ -1,2 +0,0 @@
-$(".gallery__list").slick({slidesToShow:1,slidesToScroll:3,autoplay:!0,speed:8e3,autoplaySpeed:3e3,arrows:!1});
-//# sourceMappingURL=index.bfcdc6e9.js.map
---
+++ index.d1a95db2.js
@@ -0,0 +1,2 @@
+$(".gallery__list").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,speed:4e3,autoplaySpeed:2e3,arrows:!1,cssEase:"linear",infinite:!0,fade:!0});
+//# sourceMappingURL=index.d1a95db2.js.map
