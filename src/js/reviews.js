4bce5c35b1b1d705271feae82af726939f27b811
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-09-21 19:43:34 +0100

    slide buttons update

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,16 +1,16 @@
 
 $('.reviews-list').slick({
   dots: true,
-  // direction: "horizontal",
+
   infinite: true,
   speed: 300,
   slidesToShow: 1,
   adaptiveHeight: true,
   arrows: false,
-  // autoplay: true,
-  dotsClass: 'slick-dots',
-  dots: '.reviews-button',
-
+  autoplay: true,
+  dotsClass: 'comment__choise > li',
 }
 );
+
+
 
